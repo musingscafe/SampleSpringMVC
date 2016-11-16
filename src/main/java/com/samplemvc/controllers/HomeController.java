21bbc3fdb3185e5fb2552497302bae34e56eebec
Nrapendra sharma (nrsharma) <a-nrsharma@expedia.com>
2016-11-16 16:07:45 +0530

    added query builder for mongo
    added query builder for mongo

--- src/main/java/com/samplemvc/controllers/HomeController.java
+++ src/main/java/com/samplemvc/controllers/HomeController.java
@@ -7,18 +7,25 @@ import com.mongodb.MongoClient;
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.samplemvc.model.FormData;
+import com.samplemvc.mongo.MongoQueryBuilder;
+
 import org.bson.Document;
+import org.bson.conversions.Bson;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.SystemEnvironmentPropertySource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -28,7 +35,16 @@ import java.util.Map;
 @Controller
 public class HomeController {
 
-    @RequestMapping(value="/sample", method={RequestMethod.GET})
+    private static final String MONGO_SERVER_HOST = "localhost";
+	private static final String MONGO_DB_NAME = "firstdb";
+	private static final String MONGO_COLLECTION = "testCollection";
+
+	@Autowired
+	private MongoQueryBuilder mongoQueryBuilder;
+	
+	private MongoClient mongoClient = new MongoClient( MONGO_SERVER_HOST , 27017 );
+	
+	@RequestMapping(value="/sample", method={RequestMethod.GET})
     public String homepage(Model model) {
 
         model.addAttribute("message", "Sample MVC!!!");
@@ -37,8 +53,7 @@ public class HomeController {
     }
 
     @RequestMapping(value="/rest", method={RequestMethod.GET})
-    public @ResponseBody
-    Map<String, String> sampleRestEndPoint() throws InterruptedException {
+    public @ResponseBody Map<String, String> sampleRestEndPoint() throws InterruptedException {
 
         Map<String, String> response = new HashMap<>();
         response.put("status", "success");
@@ -48,21 +63,49 @@ public class HomeController {
         return response;
     }
 
-    @RequestMapping(value="/putInMongo", method={RequestMethod.GET})
-    public @ResponseBody
-    Map<String, String> putInMongo() throws InterruptedException {
-
-        Map<String, String> response = new HashMap<>();
-        MongoClient mongoClient = new MongoClient( "localhost" , 27017 );
-        MongoDatabase db = mongoClient.getDatabase("firstdb");
-        MongoCollection<Document> coll = db.getCollection("testCollection");
-        Map<String, Object> docObject = new HashMap<>();
-        docObject.put("key1", "value1");
-        Document document = new Document(docObject);
+    @RequestMapping(value="/put", method={RequestMethod.POST})
+    public @ResponseBody Map<String, String> putInMongo(@RequestBody FormData formData) throws InterruptedException {
+        final MongoCollection<Document> coll = getMongoCollection();
+        final Document document = mongoQueryBuilder.buildPutQuery(formData);
         coll.insertOne(document);
-
         FindIterable<Document> documents = coll.find();
+        final Map<String, String> response = new HashMap<>();
         response.put("insertedObjects", documents.toString());
         return response;
+    }
+    
+    @RequestMapping(value="/search", method={RequestMethod.POST})
+    public @ResponseBody Map<String, List<String>> searchInMongo(@RequestBody FormData formData) throws InterruptedException {
+    	
+    	final MongoCollection<Document> coll = getMongoCollection();
+    	final Document filter = mongoQueryBuilder.buildSearchQuery(formData);
+    	final Map<String, List<String>> response = new HashMap<>();
+    	
+		return response;
+    	
+    }
+    
+    @RequestMapping(value="/delete", method={RequestMethod.POST})
+    public @ResponseBody String delete(@RequestBody FormData formData) throws InterruptedException {
+    	String response = "FAILURE";
+    	try {
+    		final MongoCollection<Document> coll = getMongoCollection();
+        	final Document document = mongoQueryBuilder.buildDeleteQuery(formData);
+        	Document deletedElement = coll.findOneAndDelete(document);
+        	if(deletedElement != null){
+        		response = "SUCCESS";
+        	}else{
+        		response = "RECORD NOT FOUND";
+        	}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+    	
+		return response;
+    	
+    }
+    
+    private MongoCollection<Document> getMongoCollection(){
+         return mongoClient.getDatabase(MONGO_DB_NAME).getCollection(MONGO_COLLECTION);
     }
 }
---
+++ src/main/java/com/samplemvc/model/FormData.java
@@ -0,0 +1,60 @@
+package com.samplemvc.model;
+
+public class FormData {
+
+	private String key;
+	private String operator;
+	private String searchString;
+	private String value;
+	
+	/**
+	 * @return the key
+	 */
+	public String getKey() {
+		return key;
+	}
+	/**
+	 * @param key the key to set
+	 */
+	public void setKey(String key) {
+		this.key = key;
+	}
+	/**
+	 * @return the operator
+	 */
+	public String getOperator() {
+		return operator;
+	}
+	/**
+	 * @param operator the operator to set
+	 */
+	public void setOperator(String operator) {
+		this.operator = operator;
+	}
+	/**
+	 * @return the searchString
+	 */
+	public String getSearchString() {
+		return searchString;
+	}
+	/**
+	 * @param searchString the searchString to set
+	 */
+	public void setSearchString(String searchString) {
+		this.searchString = searchString;
+	}
+	/**
+	 * @return the value
+	 */
+	public String getValue() {
+		return value;
+	}
+	/**
+	 * @param value the value to set
+	 */
+	public void setValue(String value) {
+		this.value = value;
+	}
+	
+	
+}
